23de3c2aa134ea55d1d1e49cca98e16bc18cb5a6
Gulam Ali Asgar Ansari <itzzzali4u@gmail.com>
2026-02-02 01:11:11 +0530

    remove an watermark for guidance

--- app/components/reviewSection.js
+++ app/components/reviewSection.js
@@ -300,15 +300,8 @@ const ReviewSection = () => {
                                 </div>
                             </div>
                         </div>
-
-                        {/* Swipe hint for mobile/tablet */}
-                        <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white/50 text-xs md:text-sm flex items-center gap-2 z-20 lg:hidden pointer-events-none">
-                            <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4" />
-                            </svg>
-                            <span>Swipe to navigate</span>
-                        </div>
                     </div>
+
 
                     {/* Reviews List */}
                     <div className="space-y-8">
